ce7f1be285fc6b76f5aa7c7ce1220cc0b0d06928
BraxB <braxb92@gmail.com>
2021-06-19 15:39:43 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb//localhost/fitnessTracker",
+  {
+    useNewUrlParser: true,
+  }
+);
+
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
