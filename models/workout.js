89c817d6f60c702d55af36c1cf30df0e62501c4f
BraxB <braxb92@gmail.com>
2021-06-20 22:23:39 -0400

    PUT route works after issues with model thinking exercises are strings rather than objects resolved

--- models/workout.js
+++ models/workout.js
@@ -3,21 +3,21 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  exercises: [
-    {
-      type: String,
-      name: String,
-      duration: Number,
-      distance: Number,
-      weight: Number,
-      sets: Number,
-      reps: Number,
-    },
-  ],
-  date: {
+  day: {
     type: Date,
     default: Date.now,
   },
+  exercises: [
+    {
+      type: {type: String},
+      name: {type: String},
+      duration: {type: Number},
+      distance: {type: Number},
+      weight: {type: Number},
+      reps: {type: Number},
+      sets: {type: Number},
+    },
+  ]
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
